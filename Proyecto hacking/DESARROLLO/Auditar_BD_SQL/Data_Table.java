38e6e98bca40d0e098b6d33e6ef15571cde1d24e
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2023-05-22 08:03:35 +0200

    Cambios en Auditar BD
    Se generaron comentarios en el código de ambos archivos (Auditar_BD.java y DataTable.java) para poder entenderlo de forma clara

--- Proyecto hacking/DESARROLLO/Auditar_BD_SQL/Data_Table.java
+++ Proyecto hacking/DESARROLLO/Auditar_BD_SQL/Data_Table.java
@@ -1,11 +1,13 @@
 package Auditar_BD_SQL;
 
+// Clase para representar una columna de una tabla en la base de datos
 class TableColumn {
-    private String tableName;
-    private String columnName;
-    private String dataType;
-    private String columnValue;
+    private String tableName;    // Nombre de la tabla
+    private String columnName;   // Nombre de la columna
+    private String dataType;     // Tipo de dato de la columna
+    private String columnValue;  // Valor de la columna
 
+    // Constructor de la clase
     public TableColumn(String tableName, String columnName, String dataType, String columnValue) {
         this.tableName = tableName;
         this.columnName = columnName;
@@ -13,6 +15,7 @@ class TableColumn {
         this.columnValue = columnValue;
     }
 
+    // Métodos para acceder a los atributos de la columna
     public String getTableName() {
         return tableName;
     }
