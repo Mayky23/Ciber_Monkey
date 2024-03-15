452034d2b18b6f4557364b70341f5cc0c0860d70
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2024-03-15 09:14:28 +0100

    15/03/2024
    - Se añaden comentarios para el main principal

--- CiberMonkey.py
+++ CiberMonkey.py
@@ -104,72 +104,80 @@ def switch_options(option):
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
+# Instala las dependencias necesarias para la aplicación
 def install_dependencies(dependencies):
-    clear_screen()
-    installed_packages = subprocess.check_output(['pip', 'freeze']).decode('utf-8').split('\n')
-    required_packages = [dependency.split('==')[0] for dependency in dependencies]
+    clear_screen()  
+    installed_packages = subprocess.check_output(['pip', 'freeze']).decode('utf-8').split('\n')  # Obtiene las dependencias instaladas
+    required_packages = [dependency.split('==')[0] for dependency in dependencies]  # Extrae los nombres de las dependencias requeridas
 
-    print("[-] Verificando dependencias...")
-    missing_dependencies = []
+    print("[-] Verificando dependencias...") 
+    missing_dependencies = []  # Lista para almacenar las dependencias faltantes
 
+    # Verifica si las dependencias requeridas están instaladas
     for dependency in required_packages:
         if dependency not in installed_packages:
-            missing_dependencies.append(dependency)
+            missing_dependencies.append(dependency)  # Si falta alguna, se agrega a la lista de dependencias faltantes
 
-    if not missing_dependencies:
+    if not missing_dependencies:  # Si no hay dependencias faltantes
         print(Fore.GREEN + "[-] Todas las dependencias ya están instaladas." + Style.RESET_ALL)
-        time.sleep(1)
+        time.sleep(2)  # Espera 1 segundo
     else:
-        print(Fore.YELLOW + "\n[!] Las siguientes dependencias faltan por instalar:" + Style.RESET_ALL)
-        print("____________________________________________________")
+        print(Fore.YELLOW + "\n[!] Las siguientes dependencias faltan por instalar:" + Style.RESET_ALL)  # Mensaje de dependencias faltantes
+        print("____________________________________________________")  # Línea divisoria
         
+        # Imprime las dependencias faltantes
         for dependency in missing_dependencies:
             print("\n -" ,dependency)
-        time.sleep(1)
+        time.sleep(1)  # Espera 1 segundo
 
-        total_dependencies = len(missing_dependencies)
-        progress_unit = 100 / total_dependencies
-        progress = 0
+        total_dependencies = len(missing_dependencies)  # Calcula el total de dependencias faltantes
+        progress_unit = 100 / total_dependencies  # Calcula la unidad de progreso por dependencia
+        progress = 0  # Inicializa el progreso en 0
 
+        # Instala las dependencias faltantes
         for index, dependency in enumerate(missing_dependencies, start=1):
-            print(Fore.RESET + "\nInstalando dependencia {} de {}...".format(index, total_dependencies))
+            print(Fore.RESET + "\nInstalando dependencia {} de {}...".format(index, total_dependencies))  # Mensaje de instalación de la dependencia
             try:
-                subprocess.check_output(['pip', 'install', dependency])
+                subprocess.check_output(['pip', 'install', dependency])  # Intenta instalar la dependencia utilizando pip
             except subprocess.CalledProcessError:
-                print(Fore.RED + f"No se pudo instalar la dependencia: {dependency}" + Style.RESET_ALL)
+                print(Fore.RED + f"No se pudo instalar la dependencia: {dependency}" + Style.RESET_ALL)  # Si hay un error, muestra un mensaje de error
 
-            # Simular progreso
-            time.sleep(1)
+            # Simula progreso
+            time.sleep(1)  # Espera 1 segundo
 
-            # Actualizar la barra de progreso
-            progress += progress_unit
-            bar_length = 20
-            bar = "=" * int(progress / (100 / bar_length))
-            spaces = " " * (bar_length - len(bar))
-            percent = "{}%".format(int(progress))
-            print("\rProgreso: [{}{}] {}".format(bar, spaces, percent.rjust(4)), end='', flush=True)
-            time.sleep(5)
+            # Actualiza la barra de progreso
+            progress += progress_unit  # Incrementa el progreso por la unidad de progreso
+            bar_length = 20  # Longitud de la barra de progreso
+            bar = "=" * int(progress / (100 / bar_length))  # Calcula la cantidad de "=" en la barra de progreso
+            spaces = " " * (bar_length - len(bar))  # Calcula los espacios restantes en la barra de progreso
+            percent = "{}%".format(int(progress))  # Calcula el porcentaje completado
+            print("\rProgreso: [{}{}] {}".format(bar, spaces, percent.rjust(4)), end='', flush=True)  # Imprime la barra de progreso
+            time.sleep(5)  # Espera 5 segundos
 
-        print(Fore.RESET + Back.GREEN + "\nTodas las dependencias se han instalado correctamente." + Style.RESET_ALL)
-    return True
+        print(Fore.RESET + Back.GREEN + "\nTodas las dependencias se han instalado correctamente." + Style.RESET_ALL)  # Mensaje de que todas las dependencias se han instalado correctamente
+    return True  # Retorna True cuando todas las dependencias están instaladas correctamente
 
+# Función principal
 def main():
-    dependencies = ["colorama"]
-    if install_dependencies(dependencies):
-        while True:
+    dependencies = [""]  # Lista de dependencias necesarias
+    if install_dependencies(dependencies):  # Llama a la función para instalar las dependencias y verifica si todas se instalan correctamente
+        while True: 
             try:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                clear_screen()
                 print_ascii_art()
                 print_menu()
 
-                option = int(input("\nSELECCIONA UNA OPCIÓN: " + Fore.RESET))
-                if option == 99:
-                    salir()
-                    break
-                switch_options(option)
-            except ValueError:
-                print(Fore.BLACK + Back.RED + "Por favor, ingresa un número válido." + Style.RESET_ALL)
-                continue
+                option = int(input("\nSELECCIONA UNA OPCIÓN: " + Fore.RESET))  # Solicita al usuario seleccionar una opción
+                if option == 99:  # Si la opción es 99
+                    salir()  # Llama a la función para salir
+                    break  # Rompe el bucle
+
+                switch_options(option)  # Llama a la función para manejar las opciones
+
+            except ValueError:  # Si hay un error al convertir la entrada del usuario a un entero
+                print(Fore.BLACK + Back.RED + "Por favor, ingresa un número válido." + Style.RESET_ALL)  # Muestra un mensaje de error
+                continue  # Continúa con la siguiente iteración del bucle
+
 
 
 if __name__ == "__main__":
